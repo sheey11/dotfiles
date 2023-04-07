721de3e18a6161494f4af52659ecba6d7e12d273
sheey11 <13375959+sheey11@users.noreply.github.com>
2023-04-07 12:38:01 +0800

    platform detection

--- universal/nvim/config/platform.lua
+++ universal/nvim/config/platform.lua
@@ -1,9 +1,13 @@
-if vim.fn.has('win32') then
+-- see https://github.com/neovim/neovim/issues/13971
+local sys = vim.loop.os_uname().sysname
+
+if sys == "Windows_NT" then
     vim.opt.shell = 'pwsh'
     vim.opt.shellcmdflag = '-nologo -noprofile -ExecutionPolicy RemoteSigned -command'
     vim.opt.shellxquote = ''
-end
-if vim.fn.has('mac') then
+elseif sys == "Darwin" then
     print('running on mac')
+elseif sys == "Linux" then
+    print('running on linux')
 end
 
